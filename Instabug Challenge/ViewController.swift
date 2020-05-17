fe7753af85d0e24b20d604c733eb4891c4a19827
Mostafa Wael <mostafawael7@hotmail.com>
2020-05-17 19:24:54 +0200

    Initial Commit

---
+++ Instabug Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instabug Challenge
+//
+//  Created by Mostafa Hendawi on 5/17/20.
+//  Copyright © 2020 Hendawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
